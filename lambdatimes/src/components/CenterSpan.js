bdc2cdae8262540c5b0b3b0439ac61995fa22732
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-05-17 13:04:15 -0700

    adding styling mvp complited

---
+++ lambdatimes/src/components/CenterSpan.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const CenterSpan= styled.div`
+    cursor: pointer;
+    font-weight: bold;
+    display: flex;
+    justify-content: space-between;
+    margin: 10px;
+`;
+
+export default CenterSpan
---
+++ lambdatimes/src/components/RightSpan.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const RightSpan= styled.div`
+cursor: pointer;
+margin-right: 25%;
+font-weight: bold;
+`;
+
+export default RightSpan
