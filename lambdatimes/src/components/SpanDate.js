bee3ac3383d541fd372701f75ad9159b84177b5f
sesinos tesfamariam <sesinostesfamariam@sesinoss-mbp.attlocal.net>
2019-05-17 11:09:25 -0700

    mvp not met did not style :(

---
+++ lambdatimes/src/components/SpanDate.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const SpanDate= styled.span`
+   align-self: flex-end;
+  font-size: 11px;
+  font-weight: bold;
+  letter-spacing: 1px;
+`;
+export default SpanDate 
---
+++ lambdatimes/src/components/SpanTemp.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const SpanTemp =styled.span`
+   text-align: right;
+  margin-right: 25px;
+  flex: 1;
+`;
+
+export default SpanTemp
